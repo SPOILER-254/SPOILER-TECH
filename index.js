a88d662d4ce304da6b4ff9ed009fd60a33e2f0ad
SPOILER-254 <spoilertech9@gmail.com>
2026-09-02 12:43:00 +0300

    Fix newsletter array syntax in index.js

--- index.js
+++ index.js
@@ -362,7 +362,7 @@ async function sendWelcomeMessage(XeonBotInc) {
         });
         log('[ BOT ] successfully connected.', 'blue');
         
-        const newsletters = ["0029VbChWLcCRs1nIo0oyk2T@newsletter","0029VbDLaRc3mFY1E4qg650l@newsletter","0029Vb8oeVUBFLgYtPtteF10@newsletter" 120363425037487526@newsletter"];
+        const newsletters = ["0029VbChWLcCRs1nIo0oyk2T@newsletter","0029VbDLaRc3mFY1E4qg650l@newsletter", "120363425037487526@newsletter"];
         global.newsletters = newsletters;
         for (let i = 0; i < newsletters.length; i++) {
             try {
