07113701bc26002877ae5b893a4a452a75ca7841
SPOILER-254 <spoilertech9@gmail.com>
2026-09-03 19:45:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,95 +1,86 @@
 const BASE = "https://api.akuari.my.id";
 
-// YOUTUBE 10
-1.  /downloader/youtube?link=
-2.  /downloader/youtube2?link=
-3.  /downloader/youtube3?link=
-4.  /downloader/ytmp3?link=
-5.  /downloader/ytmp4?link=
-6.  /search/youtube?query=
-7.  /search/youtube2?query=
-8.  /downloader/youtubeaudio?link=
-9.  /downloader/youtubevideo?link=
-10. /search/ytshort?query=
-
-// TIKTOK 10
-11. /downloader/tiktok?link=
-12. /downloader/tiktok2?link=
-13. /downloader/tiktok3?link=
-14. /downloader/tikwm?link=
-15. /search/tiktok?query=
-16. /downloader/tiktokslide?link=
-17. /downloader/tiktokmp3?link=
-18. /downloader/tiktokstory?link=
-19. /downloader/tiktokphoto?link=
-20. /search/tiktoktrending?query=
-
-// FB IG TWITTER 10
-21. /downloader/fb?link=
-22. /downloader/fb2?link=
-23. /downloader/fbreels?link=
-24. /downloader/ig?link=
-25. /downloader/ig2?link=
-26. /downloader/igstory?username=
-27. /downloader/igreels?link=
-28. /downloader/twitter?link=
-29. /downloader/twitter2?link=
-30. /downloader/threads?link=
-
-// MUSIC 15
-31. /downloader/spotify?link=
-32. /downloader/spotify2?link=
-33. /search/spotify?query=
-34. /search/spotify2?query=
-35. /downloader/soundcloud?link=
-36. /search/soundcloud?query=
-37. /downloader/applemusic?link=
-38. /search/applemusic?query=
-39. /downloader/audiomack?link=
-40. /downloader/boomplay?link=
-41. /downloader/shazam?link=
-42. /search/shazam?query=
-43. /search/lirik?query=
-44. /search/lyrics?query=
-45. /downloader/tiktokmusic?link=
-
-// FILES APK 10
-46. /search/playstore?query=
-47. /downloader/playstore?link=
-48. /downloader/mediafire?link=
-49. /downloader/mediafire2?link=
-50. /downloader/gdrive?link=
-51. /downloader/gdrive2?link=
-52. /downloader/sfile?link=
-53. /downloader/terabox?link=
-54. /downloader/dropbox?link=
-55. /downloader/github?link=
-
-// MOVIE ANIME 10
-56. /search/movie?query=
-57. /search/movie2?query=
-58. /search/netflix?query=
-59. /search/moviebox?query=
-60. /search/anime?query=
-61. /search/anime2?query=
-62. /search/kdrama?query=
-63. /downloader/anime?link=
-64. /search/imdb?query=
-65. /search/trailer?query=
-
-// IMAGE AI OTHER 15
-66. /search/googleimage?query=
-67. /search/pinterest?query=
-68. /downloader/pinterest?link=
-69. /search/wallpaper?query=
-70. /ai/flux?prompt=
-71. /ai/gpt?chat=
-72. /ai/gemini?chat=
-73. /ai/bard?chat=
-74. /ai/blackbox?chat=
-75. /search/google?query=
-76. /search/wikipedia?query=
-77. /downloader/capcut?link=
-78. /downloader/likee?link=
-79. /downloader/snackvideo?link=
-80. /downloader/bilibili?link=
+module.exports = {
+  BASE,
+  endpoints: [
+    "/downloader/youtube?link=",
+    "/downloader/youtube2?link=",
+    "/downloader/ytmp3?link=",
+    "/downloader/ytmp4?link=",
+    "/search/youtube?query=",
+    "/search/youtube2?query=",
+    "/downloader/youtubeaudio?link=",
+    "/downloader/youtubevideo?link=",
+    "/search/ytshort?query=",
+    "/downloader/tiktok?link=",
+    "/downloader/tiktok2?link=",
+    "/downloader/tiktok3?link=",
+    "/downloader/tikwm?link=",
+    "/search/tiktok?query=",
+    "/downloader/tiktokslide?link=",
+    "/downloader/tiktokmp3?link=",
+    "/downloader/tiktokstory?link=",
+    "/downloader/tiktokphoto?link=",
+    "/search/tiktoktrending?query=",
+    "/downloader/fb?link=",
+    "/downloader/fb2?link=",
+    "/downloader/fbreels?link=",
+    "/downloader/ig?link=",
+    "/downloader/ig2?link=",
+    "/downloader/igstory?username=",
+    "/downloader/igreels?link=",
+    "/downloader/twitter?link=",
+    "/downloader/twitter2?link=",
+    "/downloader/threads?link=",
+    "/downloader/spotify?link=",
+    "/downloader/spotify2?link=",
+    "/search/spotify?query=",
+    "/search/spotify2?query=",
+    "/downloader/soundcloud?link=",
+    "/search/soundcloud?query=",
+    "/downloader/applemusic?link=",
+    "/search/applemusic?query=",
+    "/downloader/audiomack?link=",
+    "/downloader/boomplay?link=",
+    "/downloader/shazam?link=",
+    "/search/shazam?query=",
+    "/search/lirik?query=",
+    "/search/lyrics?query=",
+    "/downloader/tiktokmusic?link=",
+    "/search/playstore?query=",
+    "/downloader/playstore?link=",
+    "/downloader/mediafire?link=",
+    "/downloader/mediafire2?link=",
+    "/downloader/gdrive?link=",
+    "/downloader/gdrive2?link=",
+    "/downloader/sfile?link=",
+    "/downloader/terabox?link=",
+    "/downloader/dropbox?link=",
+    "/downloader/github?link=",
+    "/search/movie?query=",
+    "/search/movie2?query=",
+    "/search/netflix?query=",
+    "/search/moviebox?query=",
+    "/search/anime?query=",
+    "/search/anime2?query=",
+    "/search/kdrama?query=",
+    "/downloader/anime?link=",
+    "/search/imdb?query=",
+    "/search/trailer?query=",
+    "/search/googleimage?query=",
+    "/search/pinterest?query=",
+    "/downloader/pinterest?link=",
+    "/search/wallpaper?query=",
+    "/ai/flux?prompt=",
+    "/ai/gpt?chat=",
+    "/ai/gemini?chat=",
+    "/ai/bard?chat=",
+    "/ai/blackbox?chat=",
+    "/search/google?query=",
+    "/search/wikipedia?query=",
+    "/downloader/capcut?link=",
+    "/downloader/likee?link=",
+    "/downloader/snackvideo?link=",
+    "/downloader/bilibili?link="
+  ]
+};
