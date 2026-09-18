462ae42ca2a0e14abf8915119aa815a5e8a70bea
SPOILER-254 <spoilertech9@gmail.com>
2026-09-18 09:07:31 +0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "YOUR_WHATSAPP_SESSION_ID_HERE",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "254700000000",
-  PREFIX: process.env.PREFIX || ".",
-  BOT_NAME: process.env.BOT_NAME || "SPOILER-TECH",
-  MODE: process.env.MODE || "public",
-  UPDATE_ZIP_URL: process.env.UPDATE_ZIP_URL || ""
-};
