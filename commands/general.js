95ee60aec6fa15fb43aa78dfb1edb95aba80464d
SPOILER-254 <spoilertech9@gmail.com>
2026-09-20 06:04:32 +0300

    Change module import from black_hat to spoiler-tech

--- commands/general.js
+++ commands/general.js
@@ -1,4 +1,4 @@
-const { gmd, commands, monospace, formatBytes } = require("../black_hat"),
+const { gmd, commands, monospace, formatBytes } = require("../spoiler-tech"),
   fs = require("fs"),
   axios = require("axios"),
   BOT_START_TIME = Date.now(),
