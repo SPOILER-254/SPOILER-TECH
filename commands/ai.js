1d61a19ee435feda50fcf5562292924d6d36a85c
SPOILER-254 <spoilertech9@gmail.com>
2026-09-20 05:56:07 +0300

    Update AI module to use spoiler-tech

--- commands/ai.js
+++ commands/ai.js
@@ -1,4 +1,4 @@
-const { gmd } = require("../black_hat");
+const { gmd } = require("../spoiler-tech");
 const axios = require("axios");
 
 async function queryAI(endpoint, query, conText) {
@@ -27,7 +27,7 @@ gmd(
   {
     pattern: "blackhatai",
     aliases: ["ai"],
-    description: "Chat with blackhatai AI assistant",
+    description: "Chat with spoilertechai AI assistant",
     category: "Ai",
     filename: __filename,
   },
